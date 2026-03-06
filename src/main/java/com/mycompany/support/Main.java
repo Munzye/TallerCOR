2ac68d6b78fe6b7fd9fdcc068371d70b0944ed54
Anderson Bonilla <adbonillam@udistrital.edu.co>
2026-03-05 20:08:30 -0500

    Refactor support handler chain in Main class

--- src/main/java/com/mycompany/support/Main.java
+++ src/main/java/com/mycompany/support/Main.java
@@ -1,26 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.support;
 
-import com.mycompany.support.handler.*;
-import com.mycompany.support.model.*;
+import com.mycompany.support.handler.Level1SupportHandler;
+import com.mycompany.support.handler.Level2SupportHandler;
+import com.mycompany.support.handler.Level3SupportHandler;
+import com.mycompany.support.handler.SupportHandler;
+import com.mycompany.support.model.Priority;
+import com.mycompany.support.model.Request;
 
 public class Main {
 
     public static void main(String[] args) {
-        
         SupportHandler level1 = new Level1SupportHandler();
         SupportHandler level2 = new Level2SupportHandler();
         SupportHandler level3 = new Level3SupportHandler();
 
-        //Construir correctamente la cadena
+        level1.setNextHandler(level2);
+        level2.setNextHandler(level3);
 
         Request r1 = new Request(Priority.BASIC);
         Request r2 = new Request(Priority.INTERMEDIATE);
         Request r3 = new Request(Priority.CRITICAL);
 
-        // Hacer el procesar de las solicitudes
+        level1.handleRequest(r1);
+        level1.handleRequest(r2);
+        level1.handleRequest(r3);
     }
 }
