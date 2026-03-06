7d9f792e9a94ffc15e628930972a913f53181168
Anderson Bonilla <adbonillam@udistrital.edu.co>
2026-03-05 20:05:00 -0500

    Implement Level1SupportHandler for BASIC requests

---
+++ src/main/java/com/mycompany/support/handler/Level1SupportHandler.java
@@ -0,0 +1,16 @@
+package com.mycompany.support.handler;
+
+import com.mycompany.support.model.Priority;
+import com.mycompany.support.model.Request;
+
+public class Level1SupportHandler extends SupportHandler {
+
+    @Override
+    public void handleRequest(Request request) {
+        if (request.getPriority() == Priority.BASIC) {
+            System.out.println("Nivel 1 atendiendo solicitud BASIC");
+        } else {
+            handleNext(request);
+        }
+    }
+}
