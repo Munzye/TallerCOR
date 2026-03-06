c5cf640822096877985f718941ee941836a3914c
Anderson Bonilla <adbonillam@udistrital.edu.co>
2026-03-05 19:58:31 -0500

    Enhance handleNext method with logging
    Added a log message for when no support level is available.

--- src/main/java/com/mycompany/support/handler/SupportHandler.java
+++ src/main/java/com/mycompany/support/handler/SupportHandler.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.support.handler;
 
 import com.mycompany.support.model.Request;
 
-/**
- *
- * @author DANIE
- */
 public abstract class SupportHandler {
 
     protected SupportHandler nextHandler;
@@ -19,8 +11,11 @@ public abstract class SupportHandler {
     }
 
     protected void handleNext(Request request) {
-        if(nextHandler != null) {
+        if (nextHandler != null) {
             nextHandler.handleRequest(request);
+        } else {
+            System.out.println("No existe un nivel de soporte disponible para la prioridad: "
+                    + request.getPriority());
         }
     }
 
