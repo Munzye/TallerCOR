fe33bc464537b7db79b1dd2ade8770e3f63d5eca
Anderson Bonilla <adbonillam@udistrital.edu.co>
2026-03-05 20:06:24 -0500

    Clean up Priority enum formatting
    Removed unnecessary comments and formatting.

--- src/main/java/com/mycompany/support/model/Priority.java
+++ src/main/java/com/mycompany/support/model/Priority.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.support.model;
 
-/**
- *
- * @author DANIE
- */
 public enum Priority {
-
     BASIC,
     INTERMEDIATE,
     CRITICAL
